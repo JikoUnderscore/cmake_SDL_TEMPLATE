acc86ff28892853c80e0c26b3efbfc899c4218b5
JikoUnderscore <59426055+JikoUnderscore@users.noreply.github.com>
2023-09-02 16:25:21 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,87 +4,75 @@
 #include <iostream>
 
 
-//don't do this, this is just an example
-SDL_Renderer* renderer;
-SDL_Window* window;
-bool isRunning;
-bool fullscreen;
-void handleEvents();
-void update();
-void render();
-
-
-//please don't put all your code in main like I did.
 int main() {
-
+    SDL_Renderer* renderer{};
+    SDL_Window* window{};
+    bool isRunning{ false };
+    bool fullscreen;
     fullscreen = false;
     auto flags = 0;
     flags = SDL_WINDOW_RESIZABLE;
     if (fullscreen) {
         flags = flags | SDL_WINDOW_FULLSCREEN;
     }
-    if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
-        std::cout << "Subsystems Initialized!\n";
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("SDL_Init failed: %s\n", SDL_GetError());
+        return -1;
+    }
+    printf("Subsystems Initialized!\n");
+    window = SDL_CreateWindow("Test Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, flags);
+    if (window == nullptr) {
+        printf("SDL_CreateWindow failed: %s\n", SDL_GetError());
+        return -1;
+    }
+    printf("Window Created!\n");
 
-        window = SDL_CreateWindow("Test Window", SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 500, 500, flags);
-        if (window) {
-            std::cout << "Window Created!\n";
-            SDL_SetWindowMinimumSize(window, 100, 100);
+
+    SDL_SetWindowMinimumSize(window, 100, 100);
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (renderer == nullptr) {
+        printf("SDL_CreateRenderer failed: %s\n", SDL_GetError());
+        return -1;
+    }
+    (void)SDL_SetRenderDrawColor(renderer, 121, 121, 121, 255);
+    printf("Renderer created!\n");
+    (void)SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+    isRunning = true;
+    while (isRunning) {
+        { // events
+            for (SDL_Event e{}; SDL_PollEvent(&e) == 1;) {
+                switch (e.type) {
+                    case SDL_QUIT: {
+                        printf("quiting game\n");
+                        isRunning = false;
+                    } break;
+
+                    default:
+                        break;
+                }
+            }
         }
-        renderer = SDL_CreateRenderer(window, -1, 0);
-        if (renderer) {
+
+
+        {
+            // update
+        }
+
+
+        { // render
             SDL_SetRenderDrawColor(renderer, 121, 121, 121, 255);
-            std::cout << "Renderer created!\n";
-            SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
-            isRunning = true;
+            SDL_RenderClear(renderer);
+
+            // your stuff to render would typically go here.
+            SDL_RenderPresent(renderer);
         }
-
-
-
-
     }
 
-    while(isRunning){
-        handleEvents();
-        update();
-        render();
-    }
-
-    //frees memory associated with renderer and window
+    // frees memory associated with renderer and window
     SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);	//error here
+    SDL_DestroyWindow(window); // error here
     SDL_Quit();
 
 
     return 0;
-}
-
-//handles any events that SDL noticed.
-void handleEvents(){
-    //the only event we'll check is the  SDL_QUIT event.
-    SDL_Event event;
-    SDL_PollEvent(&event);
-
-
-    switch (event.type) {
-        case SDL_QUIT:
-            isRunning = false;
-        break;
-        default:
-            break;
-    }
-}
-
-//simple render function
-void render(){
-    SDL_SetRenderDrawColor(renderer, 121,121,121, 255);
-    SDL_RenderClear(renderer);
-
-    //your stuff to render would typically go here.
-    SDL_RenderPresent(renderer);
-}
-
-//simple update function
-void update(){
-    //if things could update the code would go in here.
 }
